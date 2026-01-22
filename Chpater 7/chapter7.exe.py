4f2a1273649c285083b5cf20ccbb5b7a1b14795c
Henry974583 <henryabaier@gmail.com>
2026-01-22 13:02:47 -0600

    done

--- Chpater 7/chapter7.exe.py
+++ Chpater 7/chapter7.exe.py
@@ -1,4 +1,5 @@
 import random
+import os
 def lottery(): #exercise 2
     #accepts no arguments
     #it generates a list of random numbers
@@ -104,22 +105,120 @@ def charge_accts(): #exercise 5
     #asks for a number
     #and checks if it in a list
     #it also checks that they are valid numbers
-    accounts = []
-    try:
-       #input
-        search = int(input('Enter an account number: '))
-        #append
-        accounts.append(search)
-        #open the file
-        infile = open('charge_accounts.txt', 'r')
-        
-        account_numbers = infile.readlines()
-        
-        for  in accounts
-            print('succes')
-            
-    except:
-        print('err')
-        
-        
+    #open file and variables
+    account = []
+    go = 'y'
+    infile = open('charge_accounts.txt', 'r')
     
+    
+    if go == 'y':
+        account_number = input('type a account number ')
+        
+    while account_number.isdigit() == False:
+        account_number = input('Enter and account number(numerics only) ')
+        
+    on_list == isValid(account_number)
+    
+    if on_list == 'valid':
+        print('the account number is valid')
+    else:
+        print('The account is invalid')
+        
+    go = input('Go again? ')
+    
+    if go == 'y':
+        charge_accts()
+    
+def isValid(account_number):
+    for acc in infile:
+        acc = r.strip('\n')
+        accounts.append(acc)
+        
+    infile.close()
+    
+    if account_number not in accounts:
+        return on_list == 'invalid'
+    else:
+        return on_list == 'valid'
+    
+def drivers_exam(): #exercise 4
+    #no arguments
+    #it reads from a file
+    again = 'y'
+    total = 20
+    while again.lower() == 'y':
+        #promt the user
+        test_file = input('Please enter the name of the file ')
+        
+        if not os.path.exist(test_file):
+            print(test_file, ' not found.')
+        else:
+            #start answer key
+            answer_key = open('driver_test_key.txt', 'r')
+            
+            #open test file
+            test_answers = open(test_file, 'r')
+            
+        #lists
+            answers = []
+            user_answers = []
+            
+            #create the list
+            for line in answer_key:
+                line = line.rstip('\n')
+                user_answers.append(line)
+                
+            for line in test_answers:
+                lin = line.rstrip('\n')
+                user_answers.append(line)
+                
+            #wrong index
+                
+            wrong_index = []
+            counter = -1
+            correct = 0
+            
+            for ans in answer:
+                counter += 1
+                if ans == user_answers[counter]:
+                    correct +=1
+                else:
+                    wrong_index.append(counter + 1)
+                    
+            print('Test complete')
+            
+            print('your answerd', correct, 'questions correctly out of total', total)
+            
+            missed = total - correct
+            
+            print('You missed', missed, 'the minium you could miss to pass was 5')
+            
+            if missed > 5:
+                print('You did not pass')
+            else:
+                print('You passed')
+                
+            if missed == 0:
+                print()
+            else:
+                print('Here ar the questions you missed', wrong_index)
+                
+
+def tic_tac_toe():
+    #plays the game
+    board = [['-', '-', '-',],
+             ['-', '-', '-',],
+             ['-', '-', '-',]]
+    #print board
+    for b in board:
+        print(b)
+        
+    row = random.randint(0, 2)
+    collum = random.randint(0, 2)
+    board.remove[row][collum]
+    board.append[row][collum]
+    
+    for b in board:
+        print(b)
+    
+            
