b1dce62a96afa9a35587a0f7961f20d8359c803d
Henry974583 <henryabaier@gmail.com>
2026-01-23 12:31:43 -0600

    finsihed chapter 7

--- Chpater 7/chapter7.exe.py
+++ Chpater 7/chapter7.exe.py
@@ -205,20 +205,143 @@ def drivers_exam(): #exercise 4
                 
 
 def tic_tac_toe():
-    #plays the game
-    board = [['-', '-', '-',],
-             ['-', '-', '-',],
-             ['-', '-', '-',]]
-    #print board
-    for b in board:
-        print(b)
+    #accepts no arguments
+    #it calls everything to play tic tac toe
+     # create the list for the board
+    board = [["-", "-", "-"],
+             ["-", "-", "-"],
+             ["-", "-", "-"]]
+    x_or_o = 0
+    not_over = True
+    #loop to play
+    while not_over == True:
+        x_or_o += 1
+        index1 = random.randint(0,2)
+        index2 = random.randint(0,2)
+        while board[index1][index2] != '-':
+            index1 = random.randint(0,2)
+            index2 = random.randint(0,2)
+        #assign the place in the list a x or o
+        if x_or_o % 2 == 0:
+            letter = "X"
+        else:
+            letter = "O"
         
-    row = random.randint(0, 2)
-    collum = random.randint(0, 2)
-    board.remove[row][collum]
-    board.append[row][collum]
+        board[index1][index2] = letter
+        
+        not_over = game_over(board)
+    #display winner or not
+    winner = winner_check(board)
+    if winner != "Tie":
+        print(f"Winner is {winner}")
+    else:
+        print("It's a draw!")
+
+    print(board[0],"\n",board[1],"\n",board[2], sep = '')
+def game_over(board):
+    #recives board
+    #and chekcs if all players have been made
+    #without a winner
+    #if so it retruns true, false otherwise
+    #left adn right
+    if board[0][0] == board[0][1] and board[0][1] == board[0][2] and board[0][0] != '-':
+        return False
+    elif board[1][0] == board[1][1] and board[1][1] == board[1][2] and board[1][0] != '-':
+        return False
+    elif board[2][0] == board[2][1] and board[2][1] == board[2][2] and board[2][0] != '-':
+        return False
+    # up and down
+    elif board[0][0] == board[1][0] and board[1][0] == board[2][0] and board[0][0] != '-':
+        return False
+    elif board[0][1] == board[1][1] and board[1][1] == board[2][1] and board[0][1] != '-':
+        return False
+    elif board[0][2] == board[1][2] and board[1][2] == board[2][2] and board[0][2] != '-':
+        return False
+    # diagnals
+    elif board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[0][0] != '-':
+        return False
+    elif board[0][2] == board[1][1] and board[1][1] == board[2][0] and board[0][2] != '-':
+        return False
+    else:
+        return True
+           
+def winner_check(board):
+    # game_over recieves an argument for the board
+    # and checks if all plays have been made
+    # without a winner
+    # if so it returns true, false otherwise
+    tie = "Tie"
+        # left and right
+    if board[0][0] == board[0][1] and board[0][1] == board[0][2] and board[0][0] != '-':
+        return board[0][0]
+    elif board[1][0] == board[1][1] and board[1][1] == board[1][2] and board[1][0] != '-':
+        return board[1][0]
+    elif board[2][0] == board[2][1] and board[2][1] == board[2][2] and board[2][0] != '-':
+        return board[2][0]
+    # up and down
+    elif board[0][0] == board[1][0] and board[1][0] == board[2][0] and board[0][0] != '-':
+        return board[0][0]
+    elif board[0][1] == board[1][1] and board[1][1] == board[2][1] and board[0][1] != '-':
+        return board[0][1]
+    elif board[0][2] == board[1][2] and board[1][2] == board[2][2] and board[0][2] != '-':
+        return board[0][2]
+    # diagnals
+    elif board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[0][0] != '-':
+        return board[0][0]
+    elif board[0][2] == board[1][1] and board[1][1] == board[2][0] and board[0][2] != '-':
+        return board[0][2]
+    else:
+        return tie
+
+def white_elephant(): #exercise 5
+    #no arguments
+    #it gives a random person to a random person who they will gift
+    #outputs who can gift who
     
-    for b in board:
-        print(b)
+    #list
+    development_department = ['Julia', 'Oliver', 'Abigail']
+    hr_department = ['Camden', 'Kayleigh', 'Cooper', 'Kerrigan']
+    sales_department = ['Avery', 'Charlotte', 'Elle']
     
-            
+    #more list
+    givers = ["Julia", "Oliver", "Abigail", "Camden", "Kayleigh", "Cooper", "Kerrigan", "Avery", "Charlotte", "Elle"]
+    recievers = ["Julia", "Oliver", "Abigail", "Camden", "Kayleigh", "Cooper", "Kerrigan", "Avery", "Charlotte", "Elle"]
+    PEOPLE = ["Julia", "Oliver", "Abigail", "Camden", "Kayleigh", "Cooper", "Kerrigan", "Avery", "Charlotte", "Elle"]
+    
+    
+    print("Here are the results:")
+    #find who gets gifted to who
+    for person in PEOPLE:
+        giver = random.choice(givers)
+        reciever = random.choice(recievers)
+        while giver in development_department and reciever in development_department or giver in hr_department and reciever in hr_department or giver in sales_department and reciever in sales_department:
+            giver = random.choice(givers)
+            reciever = random.choice(recievers)
+        print(f"{giver} gifts to {reciever}")
+        #remove them from the list
+        givers.remove(giver)
+        recievers.remove(reciever)
+    
+def magic_8_ball():
+# magic 8 ball asks people for a qquestion
+# it reads from a file and adds the responses to a list
+# it prints the answer
+
+    response = open("8_ball_responses.txt", "r")
+    responses = []
+    again = 'y'
+
+    for res in response:
+        responses.append(res)
+    response.close()
+    while again == 'y':
+        input("\nWhat is your question? ")
+        print()
+        response = random.randint(0,11)
+        print(responses[response])
+        again = input("Do you want to ask another question? (y/n): ")
+
+    print('\nBeware the prophecy... Take care.')
+
+    keep_going = 'y'
+   
